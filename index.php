c13759c498edb9bc32ecf85e8083476c11e62d6b
Kenneth Ossai <kenossai1@gmail.com>
2025-03-27 20:44:34 +0000

    refactor code, moved routes to its own file.

--- index.php
+++ index.php
@@ -2,16 +2,4 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-  $route = [
-
-    '/' => 'controllers/index.php',
-
-    '/about' => 'controllers/about.php',
-
-    '/contact' => 'controllers/contact.php',
-    
-  ];
-
-
+require 'router.php';
