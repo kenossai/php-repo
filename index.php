89dda421ed6c1ff65b8451311aed1686e8ef2443
Kenneth Ossai <kenossai1@gmail.com>
2025-03-28 20:38:32 +0000

    Added a database connection

--- index.php
+++ index.php
@@ -2,4 +2,38 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+
+class Database {
+
+  public $connection;
+  public function __construct()
+  {
+    //  Connect to the database
+    $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+    $user = "root";
+    $password = "root";
+
+    $this->connection = new PDO($dsn, $user, $password);
+  }
+
+  public function query($query)
+  {
+    
+
+    $statement = $this->connection->prepare($query);
+                                                                                                                                                                                                                                                                                                                                                                                                 
+    $statement->execute();
+
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+}
+
+$db = new Database();
+
+$posts = $db->query("select * from posts");
+
+
+
+dd($posts);
